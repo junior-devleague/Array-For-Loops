3913f41736163396ce52085f50815fb3a15746cf
Christie R <creindle@hawaii.edu>
2016-11-11 00:13:55 -1000

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
-# Loops
-Jr.DevLeague Nov-10 (Saturday) Material 
+# Array-For-Loops!
+You will be learning about how to create arrays, access DOM elements, and use for loops to access the values in the arrays!
+
+## Getting Started
+1. **Fork** this respository
+2. **Clone** this repo from your personal GitHub account.
+3. Only work in the file called `main.js`
+
+#Exercises
+
+##Global variable
+Declare a variable named `count` equal to 0
+
+##increaseCount()
+You will be creating a function which will increment the count of a number in a div element.
+1. Create a variable named `countElement` that accesses the _DOM_ element `count`
+Hint: What is `count` equal to in the index.html?
+2. Assign the `countElement` innerHTML to `count`.
+3. Increment `count` by one with each click.
+
+**ADVANCED**
+1. Use a _FOR_ loop to increment `count` by 10.
+2. Use a _FOR_ loop to decrement `count` by 1.
+
+##addColor()
+You will be creating a function which will give the attribute _backgroundColor_ to the divs on the _DOM_
+1. Create an array named `colors` and set it's value to 3 different colors.
+2. Create a variable named `documentDIVs` that accesses the all the _DOM_ divs.
+Hint: You want to access the elements by their tag name
+3. Create a _FOR_ loop to iterate through `colors`.
+4. Within the _FOR_ loop, assign 1 value from `colors` to the second `documentDIVs` backgroundColor.
+Hint: Use .style.backgroundColor to give the attribute and value to the element _DOM_
+
+**ADVANCED**
+1. Can you assign each value in `colors` to each `documentDIVs` using the same _FOR_ loop?
+
+
+
+
+
+
+
--- main.js
+++ main.js
@@ -1,31 +1,41 @@
-var array = ["Bear", "Big", "Bad"];
-var colors1 = ["#0000ff", "#9932CC", "#8B008B"];
-var colors = ["#0000ff", "#9932CC", "#8B008B"];
-var size = ["0px", "10px", "10px"];
-var divs;
-
-function createDivs(){
-  var objTo = document.getElementById('container');
-  for (var i = 0; i < array.length; i++) {
-    divs = document.createElement("div");
-    divs.innerHTML = array[i];
-    objTo.appendChild(divs);
+/*
+* Function that increases the count in a box!
+*
+* @Create - Global variable "count" with value 0
+* @DOM - Create a variable named "countElement"
+* that targets the DOM element by the id "count"
+* - Have "count" equal the "innerHTML" of
+* "countElement"
+* @Do - Increment "count" by 1;
+*/
+var count = 0;
+function increaseCount(){
+  var countElement = document.getElementById("count");
+  countElement.innerHTML = count;
+  for (var i = 0; i < 10; i++){
+    count++;
   }
 }
 
+/*
+* !!!ADVANCED!!!
+*
+* Function that adds colors attributes to the divs
+* on the the DOM from an array
+*
+* @Create - Array "colors" with at least 3 color tag strings
+* @DOM - Create a variable named "documentDivs"
+* that targets the all the divs in the document
+* with "getElementsByTagName"
+* @Do - For loop iterating through "colors"
+* and assigning those values to the iteration of "documentDivs"
+* "style.backgroundColor"
+*/
 function addColor(){
-  var objTo = document.getElementById('container');
-  divs = document.getElementsByTagName("div");
-  for (var i = 0; i < colors1.length; i++) {
-    divs[i].style.backgroundColor = colors[i];
+  var colors = ["#0000ff", "#8B008B", "#9932CC"];
+  var documentDivs = document.getElementsByTagName("div");
+
+  for (var i = 0; i < colors.length; i++) {
+    documentDivs[0].style.backgroundColor = colors[i];
   }
 }
-
-function addSize(){
-  var objTo = document.getElementById('container');
-  divs = document.getElementsByTagName("div");
-  for (var i = 0; i < size.length; i++) {
-    divs[i].style.fontSize = size[i];
-  }
-}
-
